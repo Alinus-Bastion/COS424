64d8b70db69f5f9b236d66cfbe4b1f9eeab44872
Alinus-Bastion <kevinlin@princeton.edu>
2015-05-12 10:20:52 -0400

    eyp

---
+++ sprc.py
@@ -0,0 +1,22 @@
+# ==============================================================================
+# Stanford Parser Parsing
+#
+# By: Kevin Lin
+#
+
+import sys
+import subprocess
+
+def produce_Dependencies(text):
+    subprocess.call('')
+
+def setup_TrainingData():
+    
+
+def main(argv):
+    inputfile = ''
+    topic = 
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
